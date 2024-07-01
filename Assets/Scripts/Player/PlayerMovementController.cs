f6d37625f15ef71de17eb62db680d05672295c12
toukay <35756997+toukay@users.noreply.github.com>
2024-07-01 11:07:51 +0200

    Improve obstacles checking.

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -7,8 +7,13 @@ namespace Player
     public class PlayerMovementController : MonoBehaviour
     {
         [SerializeField] private LayerMask obstacleLayer;
+        [SerializeField] private string wallTag = "Wall";
+        [SerializeField] private string crateTag = "Crate";
+        [SerializeField] private string targetTag = "Target";
         
-        private readonly float _defaultDistance = 1f;
+        private const float DefaultDistance = 1f;
+        private const float RayCastOffset = DefaultDistance * 0.6f;
+        private const float RayCastDistanceMultiplier = 0.8f;
         
         private MoveCommand _moveUpCommand;
         private MoveCommand _moveDownCommand;
@@ -17,10 +22,10 @@ namespace Player
         
         private void Start()
         {
-            _moveUpCommand = new MoveCommand(this, Direction.Up, _defaultDistance);
-            _moveDownCommand = new MoveCommand(this, Direction.Down, _defaultDistance);
-            _moveLeftCommand = new MoveCommand(this, Direction.Left, _defaultDistance);
-            _moveRightCommand = new MoveCommand(this, Direction.Right, _defaultDistance);
+            _moveUpCommand = new MoveCommand(this, Direction.Up, DefaultDistance);
+            _moveDownCommand = new MoveCommand(this, Direction.Down, DefaultDistance);
+            _moveLeftCommand = new MoveCommand(this, Direction.Left, DefaultDistance);
+            _moveRightCommand = new MoveCommand(this, Direction.Right, DefaultDistance);
         }
         
         public void Move(Vector3 displacement)
@@ -60,25 +65,45 @@ namespace Player
 
         private bool IsValidMove(Vector3 displacement)
         {
-            Vector3 origin = transform.position;
             Vector3 direction = displacement.normalized;
             float distance = displacement.magnitude;
-
-            RaycastHit2D hit = Physics2D.Raycast(origin, direction, distance, obstacleLayer);
-                
-            return hit.collider == null;
+            
+            return !IsBlockedByObstacle(transform, direction, distance);
         }
+        
+        private bool CanPushCrate(GameObject crate, Vector3 direction, float distance)
+        {
+           return !IsBlockedByObstacle(crate.transform, direction, distance);
+        }
+        
+        private bool IsBlockedByObstacle(Transform originTransform, Vector3 direction, float distance)
+        {
+            Vector3 origin = originTransform.position + direction.normalized * RayCastOffset;
+
+            RaycastHit2D hit = Physics2D.Raycast(origin, direction, distance * RayCastDistanceMultiplier, obstacleLayer);
+
+            if (hit.collider == null) return false;
+            
+            GameObject hitObject = hit.collider.gameObject;
+
+            if (hitObject.CompareTag(crateTag))
+                return !CanPushCrate(hitObject, direction, distance);
+
+            return true;
+        }
+
 
         #region Editor Debugging
         #if UNITY_EDITOR
         private void OnDrawGizmos()
         {
             Vector3 origin = transform.position;
+            float raycastDistance = DefaultDistance * RayCastDistanceMultiplier;
 
-            DrawRay(origin, Vector3.up, _defaultDistance);
-            DrawRay(origin, Vector3.down, _defaultDistance);
-            DrawRay(origin, Vector3.left, _defaultDistance);
-            DrawRay(origin, Vector3.right, _defaultDistance);
+            DrawRay(origin + Vector3.up * RayCastOffset, Vector3.up, raycastDistance);
+            DrawRay(origin + Vector3.down * RayCastOffset, Vector3.down, raycastDistance);
+            DrawRay(origin + Vector3.left * RayCastOffset, Vector3.left, raycastDistance);
+            DrawRay(origin + Vector3.right * RayCastOffset, Vector3.right, raycastDistance);
         }
 
         private void DrawRay(Vector3 origin, Vector3 direction, float distance)
