2e3b373b1d074a0a2a7eb3b09fa8b5fce163875a
toukay <35756997+toukay@users.noreply.github.com>
2024-07-03 09:17:11 +0200

    Add pause screen menu to the level scene.

--- Assets/Scripts/UI/OptionsMenuController.cs
+++ Assets/Scripts/UI/OptionsMenuController.cs
@@ -32,24 +32,15 @@ namespace UI
             {
                 resolutionDropdown.options.Add(new TMP_Dropdown.OptionData($"{resolution.width} x {resolution.height}"));
             }
-            resolutionDropdown.value = Array.IndexOf(_resolutions, Screen.currentResolution);
             resolutionDropdown.RefreshShownValue();
-            fullscreenToggle.isOn = Screen.fullScreen;
-            qualityDropdown.value = QualitySettings.GetQualityLevel();
-            volumeSlider.value = audioMixer.GetFloat(volumeParameter, out float volume) ? volume : -5;
             
+            LoadPlayerSettings();
             SaveCurrentSettings();
         }
-
-        private void SaveCurrentSettings()
+        
+        public void OnOpenOptionsMenu()
         {
-            _oldSettings = new Dictionary<string, object>
-            {
-                { ResolutionKey, resolutionDropdown.value },
-                { FullScreenKey, fullscreenToggle.isOn },
-                { QualityKey, qualityDropdown.value },
-                { VolumeKey, volumeSlider.value }
-            };
+            LoadPlayerSettings();
         }
 
         public void OnDropDownResolution(int value)
@@ -75,21 +66,71 @@ namespace UI
 
         public void OnButtonApply()
         {
-            PlayerPrefs.SetInt(ResolutionKey, resolutionDropdown.value);
-            PlayerPrefs.SetInt(FullScreenKey, fullscreenToggle.isOn ? 1 : 0);
-            PlayerPrefs.SetInt(QualityKey, qualityDropdown.value);
-            PlayerPrefs.SetFloat(VolumeKey, volumeSlider.value);
-            PlayerPrefs.Save();
-            
             SaveCurrentSettings();
         }
 
         public void OnButtonCancel()
         {
+            LoadOldSettings();
+        }
+        
+        private void SaveCurrentSettings()
+        {
+            _oldSettings = new Dictionary<string, object>
+            {
+                { ResolutionKey, resolutionDropdown.value },
+                { FullScreenKey, fullscreenToggle.isOn },
+                { QualityKey, qualityDropdown.value },
+                { VolumeKey, volumeSlider.value }
+            };
+            
+            PlayerPrefs.SetInt(ResolutionKey, resolutionDropdown.value);
+            PlayerPrefs.SetInt(FullScreenKey, fullscreenToggle.isOn ? 1 : 0);
+            PlayerPrefs.SetInt(QualityKey, qualityDropdown.value);
+            PlayerPrefs.SetFloat(VolumeKey, volumeSlider.value);
+            PlayerPrefs.Save();
+        }
+        
+        private void LoadOldSettings()
+        {
+            if (_oldSettings == null) return;
+            
+            Screen.SetResolution(_resolutions[(int)_oldSettings[ResolutionKey]].width, _resolutions[(int)_oldSettings[ResolutionKey]].height, (bool)_oldSettings[FullScreenKey]);
+            Screen.fullScreen = (bool)_oldSettings[FullScreenKey];
+            QualitySettings.SetQualityLevel((int)_oldSettings[QualityKey]);
+            audioMixer.SetFloat(volumeParameter, (float)_oldSettings[VolumeKey]);
+            
             resolutionDropdown.value = (int)_oldSettings[ResolutionKey];
             fullscreenToggle.isOn = (bool)_oldSettings[FullScreenKey];
             qualityDropdown.value = (int)_oldSettings[QualityKey];
             volumeSlider.value = (float)_oldSettings[VolumeKey];
+        }
+        
+        private void LoadPlayerSettings()
+        {
+            // Load unity's default settings            
+            int defaultResolution = Array.IndexOf(_resolutions, Screen.currentResolution);
+            bool defaultFullScreen = Screen.fullScreen;
+            int defaultQualityLevel = QualitySettings.GetQualityLevel();
+            float defaultVolume = audioMixer.GetFloat(volumeParameter, out float volume) ? volume : -5;
+            
+            // Load player's saved settings (player prefs)
+            int playerResolution = PlayerPrefs.GetInt(ResolutionKey, defaultResolution);
+            bool playerFullScreen = PlayerPrefs.GetInt(FullScreenKey, defaultFullScreen ? 1 : 0) == 1;
+            int playerQualityLevel = PlayerPrefs.GetInt(QualityKey, defaultQualityLevel);
+            float playerVolume = PlayerPrefs.GetFloat(VolumeKey, defaultVolume);
+            
+            // Apply settings
+            Screen.SetResolution(_resolutions[playerResolution].width, _resolutions[playerResolution].height, playerFullScreen);
+            Screen.fullScreen = playerFullScreen;
+            QualitySettings.SetQualityLevel(playerQualityLevel);
+            audioMixer.SetFloat(volumeParameter, playerVolume);
+            
+            // Set UI elements
+            resolutionDropdown.value = playerResolution;
+            fullscreenToggle.isOn = playerFullScreen;
+            qualityDropdown.value = playerQualityLevel;
+            volumeSlider.value = playerVolume;
         }
     }
 }
---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class PauseMenuController: MonoBehaviour
+    {
+        [SerializeField] private GameObject pauseScreen;
+        [SerializeField] private GameObject pauseMenuWindow;
+        [SerializeField] private GameObject optionsMenuWindow;
+        [SerializeField] private int mainMenuSceneIndex = 0;
+
+        public static bool IsPaused = false;
+        
+        private void Start()
+        {
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1f;
+            IsPaused = false;
+        }
+
+        private void Pause()
+        {
+            pauseScreen.SetActive(true);
+            pauseMenuWindow.SetActive(true);
+            optionsMenuWindow.SetActive(false);
+            Time.timeScale = 0f;
+            IsPaused = true;
+        }
+
+        private void Resume()
+        {
+            pauseScreen.SetActive(false);
+            pauseMenuWindow.SetActive(true);
+            optionsMenuWindow.SetActive(false);
+            Time.timeScale = 1f;
+            IsPaused = false;
+        }
+
+        public void OnInputPause(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+            
+            if (IsPaused) 
+                Resume();
+            else 
+                Pause();
+        }
+        
+        public void OnButtonResume()
+        {
+            Resume();
+        }
+        
+        public void OnButtonRestart()
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+        public void OnButtonMainMenu()
+        {
+            SceneManager.LoadSceneAsync(mainMenuSceneIndex);
+        }
+    }
+}
