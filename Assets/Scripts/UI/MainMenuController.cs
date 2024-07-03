25e7ee1eec2c3c9a0a028a833bf6ba8325f664b0
toukay <35756997+toukay@users.noreply.github.com>
2024-07-03 20:38:32 +0200

    Add background and fading in effect for the main menu.

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -2,12 +2,17 @@
 using Audio;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class MainMenuController: MonoBehaviour
     {
         [SerializeField] private int gameSceneIndex = 1;
+        [SerializeField] private CanvasGroup mainMenuCanvasGroup;
+        [SerializeField] private Image background;
+        [SerializeField] private float maxBackgroundAlphaLevel = 0.7f;
+        private bool _fadeIn = false;
         
         public void Start()
         {
@@ -15,6 +20,21 @@ namespace UI
             
             if (AudioManager.Instance != null)
                 AudioManager.Instance.PlayMainMenuMusic();
+            
+            background.color = new Color(0f, 0f, 0f, 0f);
+            _fadeIn = true;
+            mainMenuCanvasGroup.alpha = 0f;
+        }
+        
+        public void Update()
+        {
+            if (_fadeIn)
+            {
+                mainMenuCanvasGroup.alpha += Time.deltaTime;
+                background.color = new Color(0f, 0f, 0f, Mathf.Min(maxBackgroundAlphaLevel, background.color.a + Time.deltaTime));
+                if (mainMenuCanvasGroup.alpha >= 1f)
+                    _fadeIn = false;
+            }
         }
         
         public void OnButtonStartGame()
